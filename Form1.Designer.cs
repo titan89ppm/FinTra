37e9e8e05c04e7e2fa3107d8d0768beb546b5a8f
titan89ppm <jon1989brown@gmail.com>
2016-04-10 10:31:27 +0100

    more files

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace startUpForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txUsername = new System.Windows.Forms.Label();
+            this.txPassword = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.txConfirmPassword = new System.Windows.Forms.Label();
+            this.tbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(186, 57);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(100, 20);
+            this.tbUsername.TabIndex = 0;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(40, 196);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(122, 23);
+            this.btnCreateAccount.TabIndex = 1;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            this.btnCreateAccount.Click += new System.EventHandler(this.btnCreateAccount_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(186, 196);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(122, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txUsername
+            // 
+            this.txUsername.AutoSize = true;
+            this.txUsername.Location = new System.Drawing.Point(70, 60);
+            this.txUsername.Name = "txUsername";
+            this.txUsername.Size = new System.Drawing.Size(55, 13);
+            this.txUsername.TabIndex = 3;
+            this.txUsername.Text = "Username";
+            // 
+            // txPassword
+            // 
+            this.txPassword.AutoSize = true;
+            this.txPassword.Location = new System.Drawing.Point(70, 86);
+            this.txPassword.Name = "txPassword";
+            this.txPassword.Size = new System.Drawing.Size(53, 13);
+            this.txPassword.TabIndex = 5;
+            this.txPassword.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(186, 83);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbPassword.TabIndex = 4;
+            // 
+            // txConfirmPassword
+            // 
+            this.txConfirmPassword.AutoSize = true;
+            this.txConfirmPassword.Location = new System.Drawing.Point(70, 112);
+            this.txConfirmPassword.Name = "txConfirmPassword";
+            this.txConfirmPassword.Size = new System.Drawing.Size(91, 13);
+            this.txConfirmPassword.TabIndex = 7;
+            this.txConfirmPassword.Text = "Confirm Password";
+            // 
+            // tbConfirmPassword
+            // 
+            this.tbConfirmPassword.Location = new System.Drawing.Point(186, 109);
+            this.tbConfirmPassword.Name = "tbConfirmPassword";
+            this.tbConfirmPassword.PasswordChar = '*';
+            this.tbConfirmPassword.Size = new System.Drawing.Size(100, 20);
+            this.tbConfirmPassword.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 262);
+            this.Controls.Add(this.txConfirmPassword);
+            this.Controls.Add(this.tbConfirmPassword);
+            this.Controls.Add(this.txPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.txUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.tbUsername);
+            this.Name = "Form1";
+            this.Text = "Account Start Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label txUsername;
+        private System.Windows.Forms.Label txPassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label txConfirmPassword;
+        private System.Windows.Forms.TextBox tbConfirmPassword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace startUpForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initialFilePathCheck();
+        }
+
+        startUp startup = new startUp();
+
+        /// <summary>
+        /// checks for available user and checks that the password is correct
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string Username = tbUsername.Text;
+            string[] passwords = { tbPassword.Text, tbConfirmPassword.Text };
+            if (isUser(Username))
+            {
+                if(checkPasswordCorrect(Username, passwords))
+                {
+                    MessageBox.Show("Thank you for logging in");
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show("Please try typing in your passwords again");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sorry this username doesn't exist, please try again");
+            }
+
+        }
+
+        /// <summary>
+        /// When create account button is clicked, the program checks that the username doesn't already exist.  If it does exist
+        /// the program won't continue.  Otherwise the folder is created and the credentials are saved in the folder
+        /// the password will also be checked for a match other wise the program wil not continue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateAccount_Click(object sender, EventArgs e)
+        {
+            if (checkBoxFill())
+            {
+                string Username = tbUsername.Text;
+                string[] passwords = { tbPassword.Text, tbConfirmPassword.Text };
+                if (checkPasswordMatch(passwords))
+                {
+                    if (checkUsername(Username))
+                    {
+                        createUser(Username);
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry this username already exists");
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Sorry These Passwords don't match");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sorry you haven't filled in all the boxes");
+            }         
+            
+        }
+
+
+
+        /// <summary>
+        /// Checks to see if the mandatory FinTra files have already been set up, if this is a new computer there will not be a directory
+        /// for the Users to be stored in (along with all other information)
+        /// This is called when form is opened
+        /// </summary>
+        private void initialFilePathCheck()
+        {
+            if (!File.Exists("C:\\FinTra"))
+            {
+                Directory.CreateDirectory("C:\\FinTra");
+            }
+            if (!File.Exists("C:\\FinTra\\Users"))
+            {
+                Directory.CreateDirectory("C:\\FinTra\\Users");
+            }
+        }
+
+
+        /// <summary>
+        /// Function to check whether the username has already been taken
+        /// </summary>
+        /// <param name="Username"></param>
+        private bool checkUsername(string Username)
+        {
+            string routePath = "C:\\FinTra\\Users\\";
+            string userPath = Path.Combine(routePath, Username);
+            if (Directory.Exists(userPath))
+            {
+                tbUsername.Clear();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        /// <summary>
+        /// Function creates a new user
+        /// </summary>
+        /// <param name="Username"></param>
+        private void createUser(string Username)
+        {
+            string routePath = "C:\\FinTra\\Users\\";        
+            string userPath = Path.Combine(routePath, Username);
+            Directory.CreateDirectory(userPath);
+            string[] credentials = { tbUsername.Text, tbPassword.Text };
+            string CredentialFormat = "- credentials.txt";
+            string userCredentials = CredentialFormat.Insert(0, Username);
+            userPath = Path.Combine(userPath, userCredentials);
+            File.WriteAllLines(userPath, credentials);
+        }
+
+
+        /// <summary>
+        /// function checks for user login
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <returns></returns>
+        private bool isUser(string Username)
+        {
+            string routePath = "C:\\FinTra\\Users\\";
+            string userPath = Path.Combine(routePath, Username);
+            if(Directory.Exists(userPath))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// function checks if the passwords match
+        /// </summary>
+        /// <param name="passwords"></param>
+        /// <returns></returns>
+        private bool checkPasswordMatch(string[] passwords)
+        {
+            if(passwords[0] != passwords[1])
+            {
+                tbConfirmPassword.Clear();
+                tbPassword.Clear();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        /// <summary>
+        /// checks the password against the requested username is correct
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <param name="Passwords"></param>
+        /// <returns></returns>
+        private bool checkPasswordCorrect(string Username, string[] Passwords)
+        {
+            if(checkPasswordMatch(Passwords))
+            {
+                StreamReader file = new StreamReader(getUserCredentialPath(Username));
+                string u = file.ReadLine();
+                string p = file.ReadLine();
+
+                if(p != Passwords[0])
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// returns file pakth for user credentials
+        /// </summary>
+        /// <param name="Username"></param>
+        /// <returns></returns>
+        private string getUserCredentialPath(string Username)
+        {
+            string routePath = "C:\\FinTra\\Users\\";
+            string userPath = Path.Combine(routePath, Username);            
+            string CredentialFormat = "- credentials.txt";
+            string userCredentials = CredentialFormat.Insert(0, Username);
+            userPath = Path.Combine(userPath, userCredentials);
+
+            return userPath;
+            
+        }
+
+        /// <summary>
+        /// checks all boxes have been filled
+        /// </summary>
+        /// <returns></returns>
+        private bool checkBoxFill()
+        {
+            if ((tbConfirmPassword.Text != "") && (tbPassword.Text != "") && (tbUsername.Text != ""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+      
+
+
+    }
+}
